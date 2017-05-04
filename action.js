a446ccc92bf68a094c127e983f7cdfb2889f4e95
swam92 <svw223@nyu.edu>
2017-05-03 23:57:34 -0400

    Added solution

--- action.js
+++ action.js
@@ -1,17 +1,17 @@
-
 function create_url(query) {
   //THIS FUNCTION SHOULD RETURN THE CONCATENATED URL (E.G. Adding all the parts together)
   var part_one = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
   var part_two = '?api-key=412adc5635b142828b22fc30c8769dd6'
   var part_three = '&q=';
-  return query;
+  return part_one + part_two + part_three + query;
 }
 
 function get_headline_and_url(response){
-  //this function should look through the first 10 and append them to the div with id=info
-  //this function returns nothing
-  //HINT 1: var headline = response.response.docs[i].headline.main;
-  //HINT 2: var link = response.response.docs[i].web_url;
+  console.log(response.response.docs[0].headline.main);
+  for (var i = 0; i <= 8; i++) {
+                var title = response.response.docs[i].headline.main;
+                $('#info').append('<div class="col-xs-3 card"' + '<p>' + title + '</p>' );
+            };
 }
 
 function is_input_valid(input){
