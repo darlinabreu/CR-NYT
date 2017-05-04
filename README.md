d6fe6318f27fd85ad0a02dad938246a6bce70c2f
swam92 <svw223@nyu.edu>
2017-05-04 07:52:33 -0400

    Updating files

---
+++ README.md
@@ -0,0 +1,3 @@
+1) Add function call on line 27
+2) What does 'create_url' do?
+3) What does like 10 do?
--- action.js
+++ action.js
@@ -1,5 +1,4 @@
 function create_url(query) {
-  //THIS FUNCTION SHOULD RETURN THE CONCATENATED URL (E.G. Adding all the parts together)
   var part_one = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
   var part_two = '?api-key=412adc5635b142828b22fc30c8769dd6'
   var part_three = '&q=';
@@ -12,7 +11,7 @@ function get_headline_and_url(response){
                 var title = response.response.docs[i].headline.main;
                 $('#info').append('<div class="col-xs-3 card"' + '<p>' + title + '</p>' );
             };
-}
+          }
 
 function is_input_valid(input){
   return true;
@@ -22,14 +21,9 @@ $(document).ready(function() {
     $('#search').click(function() {
         var query_string = $('#query').val();
         var is_valid = is_input_valid(query_string);
-        if(is_valid == true){
           var url = create_url(query_string);
           $.getJSON(url, function(response) {
-            get_headline_and_url(response);
-          }); 
-          }
-      else{
-       alert("Please enter a valid string");
-      }
+            //make function call
+          });
     });
 });
